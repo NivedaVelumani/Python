0aaf16f8f5db74527c04f450486ce9f21bb341b4
NIVEDA V <73704016+NivedaVelumani@users.noreply.github.com>
2021-05-07 22:18:17 -0700

    Create Print.py

---
+++ Print.py
@@ -0,0 +1,12 @@
+#slicing the print statement
+course='Hi  Niveda'
+print(course[1:6])
+
+#enter a input and print the statement
+ch=input('entr a che')
+print(ch)
+
+#concantinating the strings
+name=input('what is your name?')
+print("hi"+ name)
+

fa34b7c32bf6d65691fba0274da2cbd0a93b6f08
NIVEDA V <73704016+NivedaVelumani@users.noreply.github.com>
2021-05-10 07:12:15 -0700

    Create class3.py

---
+++ class3.py
@@ -0,0 +1,10 @@
+def greet_user(name):
+    print(f' "Hi {name} threwre"')
+    print("hlell")
+
+
+print("start")
+greet_user("nive")
+greet_user("prani")
+#calc_cost=(round=50,key=9,per=0.1)
+print("finsish")

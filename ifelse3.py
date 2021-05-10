5dd8e67fbd822a4bfd7168bb64ed6a3c4cb3c3d0
NIVEDA V <73704016+NivedaVelumani@users.noreply.github.com>
2021-05-10 06:51:09 -0700

    Create ifelse3.py

---
+++ ifelse3.py
@@ -0,0 +1,12 @@
+is_hot=False
+
+is_cold=False
+if is_hot:
+    print("it's a hot day")
+    print("plenty of waters")
+elif is_cold:
+    print("its a cold dau")
+    print("warn colthes")
+else:
+    print("its a lovely day")
+print("enjoy ur day")

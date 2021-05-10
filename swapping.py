c35187005c5824a599845c860461c0a1b4b89411
NIVEDA V <73704016+NivedaVelumani@users.noreply.github.com>
2021-05-10 07:24:52 -0700

    Create swapping.py

---
+++ swapping.py
@@ -0,0 +1,36 @@
+a=10
+b=11
+"""
+1st type
+
+a,b=b,a
+
+print(a)
+print(b)
+
+#2nd type
+a=a+b
+b=a-b
+a=a-b
+print(a)
+print(b)
+
+
+
+#3rd type
+
+temp=a
+a=b
+b=temp
+print(a)
+print(b)
+
+"""
+
+#4th type
+a=a^b
+b=a^b
+a=a^b
+print(a)
+print(b)
+

55e7f3e59868a0789e20960fd0a221a3779192e6
NIVEDA V <73704016+NivedaVelumani@users.noreply.github.com>
2021-05-10 07:22:23 -0700

    Create pattern2.py

---
+++ pattern2.py
@@ -0,0 +1,16 @@
+#up triangle
+""""
+for i in range(4):
+    for j in range(i+1):
+        print("# ",end="")
+
+    print()
+    
+"""""
+#down triangle
+
+for i in range(4):
+    for j in range(4-i):
+        print("# ", end="")
+
+    print()

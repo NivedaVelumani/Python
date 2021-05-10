14c591935b01fad7566ccccea5acd4ad91e062a5
NIVEDA V <73704016+NivedaVelumani@users.noreply.github.com>
2021-05-10 06:50:35 -0700

    Create ifelse2.py

---
+++ ifelse2.py
@@ -0,0 +1,9 @@
+price = 1000000
+good_credit = 10000
+Downpay=good_credit - price
+
+if(price>good_credit):
+    print("need to put dowm 10")
+else:
+    print("need to put down 20%")
+print(int(Downpay))

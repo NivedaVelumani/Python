abd9d373516ab813552be9bc094d5ceaa3d319fe
NIVEDA V <73704016+NivedaVelumani@users.noreply.github.com>
2021-05-10 07:00:47 -0700

    Create dict.py

---
+++ dict.py
@@ -0,0 +1,11 @@
+#dict are mutable
+student={
+    "name":"niveda",
+    "roll_no":"18BEE052",
+     "is_verified":True,
+
+
+}
+print(student.get("name"))
+print(student.get("name"))
+print(student["roll_no"])

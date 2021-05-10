11f6e9ac1a0176f5b66a8aa4a558c99923272ab6
NIVEDA V <73704016+NivedaVelumani@users.noreply.github.com>
2021-05-10 07:20:31 -0700

    Create palindrom.py

---
+++ palindrom.py
@@ -0,0 +1,13 @@
+n=int(input("enter a number"))
+temp =n
+rev=0
+while(n>0):
+    dig=n%10
+    rev= rev*10+dig
+    n=n//10
+
+if(temp==rev):
+    print("it a palindrom")
+
+else:
+    print("not a palindrom")

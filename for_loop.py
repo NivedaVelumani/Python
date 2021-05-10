9052e6dfa89732dc3ef24119c83c534f4a594953
NIVEDA V <73704016+NivedaVelumani@users.noreply.github.com>
2021-05-10 06:54:55 -0700

    Create for_loop.py

---
+++ for_loop.py
@@ -0,0 +1,15 @@
+#range function 
+for item in range(5,20,2):
+    print(item)
+    
+
+for x in range(3):
+    for y in range(2):
+        print((x,y))
+        
+prices=[10,20,20]
+total=0
+for item in prices:
+    total += item
+print("total prices",total)
+    
